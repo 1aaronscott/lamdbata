df5f24d912d56000f2228a3e87a5a95a3775f428
damack <damack@beast.beast>
2019-09-02 21:36:54 -0500

    Add Pipfiles, lambdata_1aarons module

---
+++ lambdata_1aarons/__init__.py
@@ -0,0 +1,9 @@
+"""
+Lambdata - collection of helper funtions for Lambda School Data Science
+"""
+
+import pandas as pd
+import numpy as np
+
+ZEROS = pd.DataFrame(np.zeros(50))
+ONES = pd.DataFrame(np.ones(10))
