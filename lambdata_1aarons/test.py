41b664ef9d7b21806e55ced444161125e5683cc7
damack <damack@beast.beast>
2019-09-02 22:17:50 -0500

    added 2 required functions for assignemtn 311

--- lambdata_1aarons/test.py
+++ lambdata_1aarons/test.py
@@ -1,6 +1,18 @@
 """
 Test file for functions to import
 """
-import pandas
+import pandas as pd
+import numpy as np
 
-DF = pandas.DataFrame([5,8,4])
+DF = pd.DataFrame([5,8,4])
+
+def TVT_split(df):
+    train, validate, test = np.split(df.sample(frac=1), [int(.6*len(df)), int(.8*len(df))])
+    # https://datascience.stackexchange.com/questions/15135/train-test-validation-set-splitting-in-sklearn
+    return train, validate, test
+
+def LIST_TO_SERIES_TO_COLUMN(list, df):
+    new_column = pd.Series( (v[0] for v in list) )
+    # https://chrisalbon.com/python/data_wrangling/pandas_join_merge_dataframe/
+    # https://stackoverflow.com/questions/21646791/convert-python-list-to-pandas-series
+    return pd.concat([df, new_column], axis=1)
---
+++ setup.py
@@ -0,0 +1,31 @@
+""" lambdata - collection of DS helper funtions
+"""
+import setuptools
+
+REQUIRED = [
+    "numpy",
+    "pandas"
+    ]
+
+with open("README.md", "r") as fh:
+    LONG_DESCRIPTION = fh.read()
+
+setuptools.setup(
+    name="lambdata_1aarons",
+    version="0.0.1",
+    author="1aarons",
+    description="Collection of DS helper functions",
+    long_description=LONG_DESCRIPTION,
+    long_description_content_type="text/markdown",
+    url="https://github.com/1aaronscott/lamdbata",
+    packages=setuptools.find_packages(),
+    python_requires=">-3.5",
+    install_requires=REQUIRED,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+        ],
+    )
+    
+    
