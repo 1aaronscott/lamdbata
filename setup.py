b5b35d5752772d839e65cd5e41cbe3627366cd53
AaronS <1aaronscott@gmail.com>
2019-09-05 20:12:05 -0500

    bump setup.py version number

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="lambdata_1aarons",
-    version="0.0.5",
+    version="0.0.6",
     author="1aarons",
     description="Collection of DS helper functions",
     long_description=LONG_DESCRIPTION,
